07930321f5a3c1f671f37f4657e42cb819ad9091
palden dorje titung <paldendorjetitung@gmail.com>
2025-03-12 09:51:27 +0545

    practce

--- pattern/pattern7.c
+++ pattern/pattern7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n = 4,i,j; // Number of rows in the upper part of the diamond
+    int n = 4,i,j; 
 
     // Upper part of the diamond
     for ( i = 0; i < n; i++) {
@@ -9,20 +9,20 @@ int main() {
         for ( j = 0; j < n - i - 1; j++) {
             printf("  ");
         }
-        // Print asterisks
+
         for ( j = 0; j <= i; j++) {
             printf("* ");
         }
         printf("\n");
     }
 
-    // Lower part of the diamond
+
     for ( i = n - 1; i > 0; i--) {
-        // Print leading spaces
+
         for (j = 0; j < n - i; j++) {
             printf("  ");
         }
-        // Print asterisks
+    
         for ( j = 0; j < i; j++) {
             printf("* ");
         }
